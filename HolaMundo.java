da084933992655b4fcc57d0a361edf04533b4a08
yefer25-coder <gyefersonalejandro@gmail.com>
2025-09-08 21:25:06 -0500

    first: Create HolaMundo file

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+
+        String nombre = "Yeferson";
+        System.out.println("¡Hola mundo!... y hola " + nombre);
+        
+    }
+}
