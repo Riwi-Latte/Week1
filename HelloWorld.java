c87eadd65f8d3cf74fed0304c4b2bc644ebeea83
Jteban1 <sepesteban@gmail.com>
2025-09-08 21:16:01 -0500

    feat: Hello World Java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    String name = "Juan";
+
+    public static void main(String[] args) {
+        String username = "Juan";
+        // HelloWorld hello = new HelloWorld();
+        System.out.println("Hello " + username);
+    }
+}
