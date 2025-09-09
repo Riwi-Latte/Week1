0367fd0926f5a60eb34d7089806c8b3adb0e6243
isabellaPulgarin <isabellapul7@gmail.com>
2025-09-08 21:19:10 -0500

    feat: create file java, isabella

---
+++ java-isa/helloWorld.java
@@ -0,0 +1,10 @@
+public class helloWorld{
+
+
+    public static void main(String[] args){
+        
+        String nombre = "Isabella";
+        System.out.println("Hello World " + nombre);
+    }
+    
+}
