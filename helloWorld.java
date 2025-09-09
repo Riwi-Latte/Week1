0ce1a22855a8a6fc951c074848b5a5e9ea19b6ac
Oomass7 <loaizarodriueztomas@gmail.com>
2025-09-08 21:16:30 -0500

    feat: Create helloWorld file

---
+++ helloWorld.java
@@ -0,0 +1,8 @@
+public class helloWorld {
+
+    public static void main(String[] args) {
+        String name = "Tomas Loaiza";
+        System.out.println("Hello " + name);
+    }
+    
+}
--- testing_jdk.java
+++
@@ -1,7 +0,0 @@
-public class testing_jdk {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-    
-}
