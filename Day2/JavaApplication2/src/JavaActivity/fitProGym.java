4c1b9a39f5d93b75b91bbd8fb81ba11d304530cf
Oomass7 <loaizarodrigueztomas@gmail.com>
2025-09-10 12:47:55 -0500

    feat: create file for the gym activity

---
+++ Day2/JavaApplication2/src/JavaActivity/fitProGym.java
@@ -0,0 +1,33 @@
+package JavaActivity;
+
+import java.util.Scanner;
+
+public class fitProGym {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void data(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = input.nextLine();
+        System.out.println("How old are you?");
+        int age = input.nextInt();
+        System.out.println("How tall are you?");
+        double height = input.nextDouble();
+        System.out.println("how weight are you?");
+        double weigth = input.nextDouble();
+        System.out.println("What's your plan? (Basic, Plus, Premium)");
+        String plan = input.nextLine();
+        System.out.println("Is your first time?");
+        boolean firstTime = input.nextBoolean();
+    }
+
+    public static void discounts(String[] args) {
+        data(args);
+
+        
+    }
+}
